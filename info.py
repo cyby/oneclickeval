58ec405ec4343970dd1aefd95695f8d34c58fda3
Makoto P. Kato <kato@dl.kuis.kyoto-u.ac.jp>
2014-03-04 16:12:52 +0900

    Add files

---
+++ info.py
@@ -0,0 +1,12 @@
+# package information.
+INFO = dict(
+  name        = "OneClickEval",
+  description = "Evaluation tool for 1CLICK task",
+  author      = "Makoto P. Kato",
+  author_email = "kato@dl.kuis.kyoto-u.ac.jp",
+  license     = "MIT License",
+  url         = "https://github.com/cyby/oneclickeval/",
+  classifiers = [
+    "Programming Language :: Python :: 2.7"
+  ]
+)
